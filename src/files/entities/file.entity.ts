1d2547ddb2d47af83fb55005464c76f6775d18ea
Anderson <andersonbernal2017@gmail.com>
2023-09-17 15:47:40 -0400

    feat(entity): add FileTypeOrm entity

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,8 +1,31 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
 export class File {
     id: string;
     mime_type: string;
     hash: string;
     buffer: Buffer;
     createdAt: Date;
+    updatedAt: Date;
+}
+
+@Entity()
+export class FileTypeOrm implements File {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    mime_type: string;
+
+    @Column()
+    hash: string;
+
+    @Column('bytea')
+    buffer: Buffer;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
     updatedAt: Date;
 }
