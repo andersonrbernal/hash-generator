fc8a42d31c20ce5a019da4f605792e75bc4203d7
Anderson <andersonbernal2017@gmail.com>
2023-09-21 15:58:42 -0400

    chore: add view engine setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { APP_BASE } from './config/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(APP_BASE.PUBLIC_DIR);
+  app.setBaseViewsDir(APP_BASE.VIEWS_DIR);
+  app.setViewEngine(APP_BASE.VIEW_ENGINE);
   await app.listen(3000);
 }
 bootstrap();
