0ddaaf167b8d4e0cfca54fb9c5c6cb33cf342a6a
Anderson <andersonbernal2017@gmail.com>
2023-09-21 16:01:03 -0400

    feat(module): update app module
    Added all dependent module to setup up the database and the files module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
+import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { FilesModule } from './apps/files/files.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, FilesModule, ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController],
+  providers: [ConfigService],
+  exports: [DatabaseModule]
 })
 export class AppModule { }
