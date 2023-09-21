4f7a77894018d72633b6f11fbb89cee36061f2d0
Anderson <andersonbernal2017@gmail.com>
2023-09-21 14:37:05 -0400

    feat(module): add files module

---
+++ src/apps/files/files.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { DatabaseModule } from "src/database/database.module";
+import { FilesController } from "./files.controller";
+import { fileProviders } from "./files.providers";
+import { FilesService } from "./files.service";
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [FilesController],
+  providers: [...fileProviders, FilesService]
+})
+export class FilesModule { }
