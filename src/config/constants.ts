79ea614aee304b301705a957bd8aa56cc80085bf
Anderson <andersonbernal2017@gmail.com>
2023-09-21 15:59:29 -0400

    chore: add APP_BASE constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,12 @@
+import { join } from "path";
+
 export const REPOSITORIES = {
     FILE: 'FILE_REPOSITORY',
     DATA_SOURCE: 'DATA_SOURCE',
 }
+
+export const APP_BASE = {
+    VIEW_ENGINE: 'hbs',
+    PUBLIC_DIR: join(__dirname, '..', '..', 'public'),
+    VIEWS_DIR: join(__dirname, '..', '..', 'views'),
+};
