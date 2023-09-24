ce52278a42da2495a1f03babc31e9b598143a826
Anderson <andersonbernal2017@gmail.com>
2023-09-24 16:42:29 -0400

    chore(18n): import I18nModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { FilesModule } from './apps/files/files.module';
+import { I18nModule } from './i18n/i18n.module';
 
 @Module({
-  imports: [DatabaseModule, FilesModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DatabaseModule, FilesModule, ConfigModule.forRoot({ isGlobal: true }), I18nModule],
   controllers: [AppController],
   providers: [ConfigService],
   exports: [DatabaseModule]
